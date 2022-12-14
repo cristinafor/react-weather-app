9f20113948f6c4370b2d923729663c5243ff41eb
Cristina F <77860767+cristinafor@users.noreply.github.com>
2022-12-14 22:53:53 +0100

    Added app html structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://www.linkedin.com/in/cristinafornasier/">
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Berlin</h1>
+      <ul>
+        <li>Wednesday 22:00</li>
+        <li>Clear</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Clear"
+          />
+          -8°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 3%</li>
+            <li>Humidity: 84%</li>
+            <li>Wind: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
